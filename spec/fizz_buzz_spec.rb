e6548967854a4c8b929fb325f77dbee8524623d9
odeane_croney <odeane_croney@yahoo.com>
2020-05-23 20:47:44 +0200

    creating fizzbuzz challenge

--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -1,8 +1,17 @@
 require './lib/fizz_buzz'
 
-describe 'fizz buzzzzz' do
+describe 'fizz_buzz' do
     
-    it "returns 1 if number is 1" do
-        expect(fizz_buzz(1)).to eq 1
+    it "returns 'fizz' if number is divisible by 15" do
+        expect(fizz_buzz(15)).to eq 'Fizz'
+      end
+       
+
+    it "returns 'buzz' if number is divisible by 5" do
+        expect(fizz_buzz(5)).to eq 'Buzz'
     end
+
+    it "returns 'buzz if number is divisible by 3"
+    expect(fizz_buzz(3)).to eq 'fizzBuzz'
 end
+
